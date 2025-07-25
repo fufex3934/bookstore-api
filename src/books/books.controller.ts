1824ac13d7645de7b8a5f7bbf161c882928d2d5b
fufex3934 <fufawakgari174@gmail.com>
2025-07-26 01:07:30 +0300

    feat:generate books service

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  @Get()
+  findAll() {
+    return 'This action returns all books';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return ` This action returns book with Id:${id}`;
+  }
+
+  @Post()
+  create(@Body() createBookDto: any) {
+    return ` This action adds a new book with title: ${createBookDto}`;
+  }
+}
