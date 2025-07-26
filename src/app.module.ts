9a7ec41d255fe43a78c645b5e242a3ab9033b7d9
fufex3934 <fufawakgari174@gmail.com>
2025-07-26 16:43:33 +0300

    generate user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,23 @@ import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { LoggingMiddleware } from './logging/logging.middleware';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/bookstore'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+    }),
     BooksModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
