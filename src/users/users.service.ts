208136fbcc9cf5ecadd524a7df0b92214f843913
fufex3934 <fufawakgari174@gmail.com>
2025-07-26 17:09:09 +0300

    register users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
-
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(registerDto: RegisterDto): Promise<Omit<User, 'password'>> {
+    const existingUser = await this.userModel
+      .findOne({ email: registerDto.email })
+      .exec();
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    const createdUser = new this.userModel({
+      ...registerDto,
+      password: hashedPassword,
+    });
+    const savedUser = await createdUser.save();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = savedUser.toObject();
+    return result;
+  }
+}
