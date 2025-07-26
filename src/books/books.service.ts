f71477a31c5794a9d6bf13290e7b0e1d1950d5b2
fufex3934 <fufawakgari174@gmail.com>
2025-07-26 10:52:03 +0300

    define interface

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Books } from './interfaces/books.interface';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  private readonly books: Books[] = [];
+
+  create(book: Books) {
+    this.books.push(book);
+  }
+
+  findAll(): Books[] {
+    return this.books;
+  }
+}
---
+++ src/books/interfaces/books.interface.ts
@@ -0,0 +1,6 @@
+export interface Books {
+  title: string;
+  description: string;
+  author: string;
+  publishedDate: Date;
+}
